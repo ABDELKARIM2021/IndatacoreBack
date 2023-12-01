1f6279c08d1d188d2a3464a730d790c96e06f25b
ABDELKARIM2021 <elouaddany.k99@gmail.com>
2023-12-01 16:47:28 +0100

    firstCommit

---
+++ src/main/java/com/example/IndatacoreBack/IndatacoreBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.IndatacoreBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndatacoreBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndatacoreBackApplication.class, args);
+	}
+
+}
